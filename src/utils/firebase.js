91a0dc2351eddfa33a17a89889ec3d6b431dbb61
ViniciusNF <cibivinibivi@gmail.com>
2026-06-12 20:18:24 -0300

    alsdal

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, addDoc, query, where, orderBy, limit, getDocs, serverTimestamp, Timestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const ip_publico = '18.228.43.111';
+const ip_publico = '54.207.202.194';
 
 const firebaseConfig = JSON.parse(atob("ew0KICAgICJhcGlLZXkiOiAiQUl6YVN5RGdGeTk1eFlKZ2tHMVdlQlN1a0M0eGF0UUwzaDBheEQ0IiwNCiAgICAiYXV0aERvbWFpbiI6ICJzcGFjZS13YXJzLTYxZDI2LmZpcmViYXNlYXBwLmNvbSIsDQogICAgInByb2plY3RJZCI6ICJzcGFjZS13YXJzLTYxZDI2IiwNCiAgICAic3RvcmFnZUJ1Y2tldCI6ICJzcGFjZS13YXJzLTYxZDI2LmZpcmViYXNlc3RvcmFnZS5hcHAiLA0KICAgICJtZXNzYWdpbmdTZW5kZXJJZCI6ICI2MzI5ODE0NzI3ODUiLA0KICAgICJhcHBJZCI6ICIxOjYzMjk4MTQ3Mjc4NTp3ZWI6MzA1ZWRiZjU5YjZhN2IzYzlmNTNiMCINCn0="));
 
